e3df96d88dc63c9f62fca82c09d24cdc58beb16f
Pkaynan <pietrokaynan1@gmail.com>
2025-09-22 20:10:21 -0300

    criando model ficha

---
+++ src/main/java/bem_estar_animal/tcc/model/ficha.java
@@ -0,0 +1,49 @@
+package bem_estar_animal.tcc.model;
+
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class ficha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    private String processo;
+
+    private String recebido_por;
+
+    private Instant data;
+
+    private Instant hora;
+
+    private String denunciante_id;
+
+    private String assunto;
+
+    private String desfecho_da_notificacao;
+
+    private String data_tramite;
+
+    private String hora_tramite;
+
+    private String funcionario_id;
+
+    private String historico;
+
+    private String animal;
+
+}
