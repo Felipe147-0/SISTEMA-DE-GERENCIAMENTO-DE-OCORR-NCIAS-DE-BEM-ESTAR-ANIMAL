e398f4384bf6fae3e94fccb6994d5e83362f41df
Pkaynan <pietrokaynan1@gmail.com>
2025-09-22 19:55:41 -0300

    first commit - projeto vazio

---
+++ src/main/java/bem_estar_animal/tcc/BemEstarAnimalApplication.java
@@ -0,0 +1,13 @@
+package bem_estar_animal.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemEstarAnimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemEstarAnimalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bem_estar_animal/tcc/BemEstarAnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package bem_estar_animal.tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemEstarAnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
