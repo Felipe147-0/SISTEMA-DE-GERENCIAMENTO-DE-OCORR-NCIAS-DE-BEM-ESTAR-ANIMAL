283ce0ddb3a15c2d921f479a7fd8f9a39c28af12
Pkaynan <pietrokaynan1@gmail.com>
2025-09-22 20:13:48 -0300

    criando FichaRepository

---
+++ src/main/java/bem_estar_animal/tcc/repository/FichaRepository.java
@@ -0,0 +1,11 @@
+package bem_estar_animal.tcc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import bem_estar_animal.tcc.model.Ficha;
+
+@Repository
+public interface FichaRepository extends JpaRepository<Ficha, Long>{
+
+}
