bb3d3e36bcea4efa098553872cce1edebabd542b
Pkaynan <pietrokaynan1@gmail.com>
2025-09-22 20:53:03 -0300

    criando Funcionario MVC

---
+++ src/main/java/bem_estar_animal/tcc/model/Funcionario.java
@@ -0,0 +1,29 @@
+package bem_estar_animal.tcc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long funcionario_id;
+
+    private String nome;
+
+    private String registro;
+
+    private String funcao;
+
+}
