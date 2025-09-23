4f2e2d30fc1eb2b9ee61e15c45191ec95167964b
Pkaynan <pietrokaynan1@gmail.com>
2025-09-22 21:06:58 -0300

    criando Login MVC

---
+++ src/main/java/bem_estar_animal/tcc/model/Login.java
@@ -0,0 +1,28 @@
+package bem_estar_animal.tcc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Login {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long longin_id;
+
+    private String username;
+
+    private String password;
+
+    private String funcionario_id;
+}
---
+++ src/main/java/bem_estar_animal/tcc/repository/LoginRepository.java
@@ -0,0 +1,11 @@
+package bem_estar_animal.tcc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import bem_estar_animal.tcc.model.Login;
+
+@Repository
+public interface LoginRepository extends JpaRepository<Login, Long>{
+
+}
