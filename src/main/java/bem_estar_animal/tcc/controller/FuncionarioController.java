137a2869a601ad6d2e3c3a5d944214dc829f4c38
Pkaynan <pietrokaynan1@gmail.com>
2025-09-28 01:36:39 -0300

    Funcionario - CRUD completo

--- src/main/java/bem_estar_animal/tcc/controller/FuncionarioController.java
+++ src/main/java/bem_estar_animal/tcc/controller/FuncionarioController.java
@@ -3,8 +3,61 @@ package bem_estar_animal.tcc.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import bem_estar_animal.tcc.model.Funcionario;
+import bem_estar_animal.tcc.record.FuncionarioRecord;
+import bem_estar_animal.tcc.service.FuncionarioService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 @RestController
 @RequestMapping("/funcionario")
 public class FuncionarioController {
+
+    private FuncionarioService funcionarioService;
+
+    public FuncionarioController(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Funcionario>> getAllFuncionarios() {
+        List<Funcionario> funcionarioList = funcionarioService.getAllFuncionarios();
+        return ResponseEntity.ok().body(funcionarioList);
+    }
+
+    @PostMapping("/criar/{id}")
+    public ResponseEntity<Funcionario> createFuncionario(@PathVariable Long id,
+            @RequestBody FuncionarioRecord funcionarioRecord) {
+
+        Funcionario funcionario = funcionarioService.createFuncionario(id, funcionarioRecord);
+        return ResponseEntity.ok().body(funcionario);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Funcionario> updateFuncionario(@PathVariable Long id,
+            @RequestBody FuncionarioRecord funcionarioRecord) {
+
+        Funcionario funcionario = funcionarioService.updateFuncionario(id, funcionarioRecord);
+        return ResponseEntity.ok().body(funcionario);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteFuncionario(@PathVariable Long id) {
+        boolean deletedFuncionario = funcionarioService.deleteFuncionario(id);
+
+        if (deletedFuncionario) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok().build();
+    }
 
 }
---
+++ src/main/java/bem_estar_animal/tcc/record/FuncionarioRecord.java
@@ -0,0 +1,5 @@
+package bem_estar_animal.tcc.record;
+
+public record FuncionarioRecord(String nome, String registro, String funcao, Long loginId) {
+
+}
