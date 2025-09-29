fb46af64802f90b617662e51a9eb3997f2275380
Pkaynan <pietrokaynan1@gmail.com>
2025-09-29 08:39:13 -0300

    adicionando controladores para as rotas: denunciante, endereco, ficha, funcionario e login do thymeleaf

---
+++ src/main/java/bem_estar_animal/tcc/thymeleaf/DenuncianteController.java
@@ -0,0 +1,10 @@
+package bem_estar_animal.tcc.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/denunciante")
+public class DenuncianteController {
+
+}
