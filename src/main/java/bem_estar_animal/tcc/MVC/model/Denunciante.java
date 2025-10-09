b26a0ac4ca123c0a344cce5240edcc1e2c77c6b0
Pkaynan <pietrokaynan1@gmail.com>
2025-10-08 22:52:59 -0300

    adicionando tostring e adicionando as primeiras funções

--- src/main/java/bem_estar_animal/tcc/MVC/model/Denunciante.java
+++ src/main/java/bem_estar_animal/tcc/MVC/model/Denunciante.java
@@ -14,9 +14,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Getter
 @Setter
 @Entity
