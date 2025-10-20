09e831b302b1985bee874d4cc0652a402a0fb746
Pkaynan <pietrokaynan1@gmail.com>
2025-10-20 15:40:05 -0300

    ouvidoria home e consulta de protocolos

---
+++ src/main/java/bem_estar_animal/tcc/thymeleaf/OuvidoriaController.java
@@ -0,0 +1,23 @@
+package bem_estar_animal.tcc.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+@RequestMapping("/ouvidoria")
+public class OuvidoriaController {
+
+    @GetMapping()
+    public String viewHome() {
+        return "ouvidoria-home";
+    }
+    
+    @GetMapping("/protocolos")
+    public String viewProtocolos() {
+        return "ouvidoria-consultarProtocolos";
+    }
+    
+}
