546ed1aa653dd522aa210bd702b3d7a04167bbe2
Pkaynan <pietrokaynan1@gmail.com>
2025-10-24 01:17:45 -0300

    adicionando links e botoes

--- src/main/java/bem_estar_animal/tcc/thymeleaf/OuvidoriaController.java
+++ src/main/java/bem_estar_animal/tcc/thymeleaf/OuvidoriaController.java
@@ -12,4 +12,10 @@ public class OuvidoriaController {
     public String viewHome() {
         return "ouvidoria-home";
     }
+
+    @GetMapping("/protocolos")
+    public String constultarProtocolos() {
+        return "ouvidoria-consultarProtocolos";
+    }
+    
 }
